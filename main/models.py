ebf2783df6e1a4d2135a33922312a1b3b6c83b4c
Quuton <davidmatthewantonio@gmail.com>
2024-04-28 17:08:58 +0800

    App Setup

---
+++ main/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# class BenefitNews(models.Model):
+#     # Information provided by the owner
+#     number = models.CharField(max_length=20)
+#     description = models.TextField(blank = True)
+#     floor = models.IntegerField()
+#     type = models.CharField(max_length=50)
+#     image = models.ImageField(upload_to='room_images',blank = True, default='room_images/placeholder.png')
+#     size = models.IntegerField()
+#     price = models.FloatField()
+#     availability = models.BooleanField(default = True)
+
+#     # Fields handled by the server
+#     visitCount = models.IntegerField(default = 0)
+#     lastUpdated = models.DateTimeField(auto_now = True)
+
+
+# Create your models here.
